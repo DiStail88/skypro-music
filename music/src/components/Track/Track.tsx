803449045870bab7909b3dbf5cb70e56cb14eca3
DiStail88 <lebwebslav@gmail.com>
2025-08-11 18:24:56 +0300

    Добавил точку

--- music/src/components/Track/Track.tsx
+++ music/src/components/Track/Track.tsx
@@ -3,8 +3,8 @@
 import Link from 'next/link';
 import style from './track.module.css';
 import { formatTime } from '@/utils/helper';
-import { useAppDispatch } from '@/store/store';
-import { setCurrentTrack, setIsPlay } from '@/store/features/trackSlice';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { setCurrentTrack } from '@/store/features/trackSlice';
 
 type TrackProps = {
   track: TrackType;
@@ -12,10 +12,14 @@ type TrackProps = {
 
 export default function Track({ track }: TrackProps) {
   const dispatch = useAppDispatch();
+  const { currentTrack, isPlay } = useAppSelector((state) => state.tracks);
 
   const onClickTrack = () => {
     dispatch(setCurrentTrack(track));
   };
+
+  const isCurrent = currentTrack?._id === track._id;
+
   return (
     <div
       key={track._id}
@@ -24,11 +28,24 @@ export default function Track({ track }: TrackProps) {
     >
       <div className={style.playlist__track}>
         <div className={style.track__title}>
-          <div className={style.track__titleImage}>
+          <div
+            className={style.track__titleImage}
+            style={{ position: 'relative' }}
+          >
             <svg className={style.track__titleSvg}>
               <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
             </svg>
+
+            {/* Точка */}
+            {isCurrent && (
+              <span
+                className={`${style.track__playingDot} ${
+                  isPlay ? style.pulsing : ''
+                }`}
+              ></span>
+            )}
           </div>
+
           <div className="track__title-text">
             <Link className={style.track__titleLink} href="">
               {track.name}
